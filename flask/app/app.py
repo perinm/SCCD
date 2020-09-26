d3d84e356b01f913c4da7d6a8473e15359b4153d
perinm <lucasperinm@gmail.com>
2020-09-26 14:01:49 +0000

    Added /soma/n1_n2
    Added func to sum n1 and n2

--- flask/app/app.py
+++ flask/app/app.py
@@ -11,5 +11,9 @@ def hora():
 	import time
 	return time.strftime("%m/%d/%Y, %H:%M:%S")
 
+@app.route('/soma/<int:n1>_<int:n2>')
+def soma(n1,n2):
+	return f'{n1+n2}'
+
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", debug=True)
