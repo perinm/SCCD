f47a2456ea338cd21ee4b3bef8f1746fb6e0254c
Lucas Manchine <lucas@localhost>
2020-10-10 13:40:42 +0000

    Fixed every problem
    There were some problems related to wrong assignments of localhost ip's adresses

--- ProvaP1/Dockerbuild/entrypoint.sh
+++ ProvaP1/Dockerbuild/entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 service ssh start
+#if [ -z "$(ssh-keygen -F $IP)" ]; then
+#  ssh-keyscan -H $IP >> ~/.ssh/known_hosts
+#fi
+ssh-keyscan -H localhost >> ~/.ssh/known_hosts
+ssh-keyscan -H $(hostname) >> ~/.ssh/known_hosts
 $HADOOP_HOME/sbin/start-dfs.sh
 $HADOOP_HOME/sbin/start-yarn.sh
 
